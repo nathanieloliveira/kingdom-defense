a1619288669227937dce6a0f0fb6b49bd4a5e99f
Nathaniel Salvador de Oliveira <nathanieloliveira@yahoo.com.br>
2025-07-27 22:04:30 -0300

    removed imgui, added cimgui, build with sdl3 gpu backend

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+
 #include <SDL3/SDL.h>
+
+#include <cimgui.h>
+#include <cimgui_impl.h>
 
 #define SDL_PRINT_ERROR_AND_EXIT(_err)                \
     do {                                              \
@@ -20,7 +25,7 @@ int main(void) {
         SDL_PRINT_ERROR_AND_EXIT("Failed to create window");
     }
 
-    SDL_GPUDevice *gpu = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, true, nullptr);
+    SDL_GPUDevice *gpu = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, true, NULL);
     if (!gpu) {
         SDL_PRINT_ERROR_AND_EXIT("Create gpu failed");
     }
@@ -29,11 +34,24 @@ int main(void) {
         SDL_PRINT_ERROR_AND_EXIT("Failed to claim window for gpu device");
     }
 
+    igCreateContext(NULL);
+    igStyleColorsDark(NULL);
+
+    ImGui_ImplSDL3_InitForSDLGPU(window);
+    ImGui_ImplSDLGPU3_InitInfo imgui_init_info = {
+            .Device = gpu,
+            .MSAASamples = 4,
+            .ColorTargetFormat = SDL_GetGPUSwapchainTextureFormat(gpu, window),
+    };
+    ImGui_ImplSDLGPU3_Init(&imgui_init_info);
+
     bool quit = false;
 
     while (!quit) {
         SDL_Event e;
         while (SDL_PollEvent(&e)) {
+            ImGui_ImplSDL3_ProcessEvent(&e);
+
             if (e.type == SDL_EVENT_QUIT) {
                 quit = true;
             }
@@ -49,21 +67,52 @@ int main(void) {
             SDL_PRINT_ERROR_AND_EXIT("Failed to acquire GPU Swapchain texture");
         }
 
-        SDL_GPURenderPass *render_pass = SDL_BeginGPURenderPass(command_buffer, &((SDL_GPUColorTargetInfo) {
-            .texture = window_texture,
+        SDL_GPUColorTargetInfo target_info = {
+                .texture = window_texture,
                 .clear_color = (SDL_FColor) { .r = 0.1f, .g = 0.1f, .b = 0.1f, .a = 1.0f },
                 .mip_level = 0,
                 .load_op = SDL_GPU_LOADOP_CLEAR,
                 .store_op = SDL_GPU_STOREOP_STORE,
-        }), 1, nullptr);
-
+        };
+        SDL_GPURenderPass *render_pass = SDL_BeginGPURenderPass(command_buffer, &target_info, 1, NULL);
         SDL_EndGPURenderPass(render_pass);
+
+        // draw imgui
+        ImGui_ImplSDLGPU3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        igNewFrame();
+
+        {
+            bool open = true;
+            igBegin("Some imGui window", &open, 0);
+            igText("something here");
+            igEnd();
+        }
+
+        igRender();
+        ImDrawData *draw_data = igGetDrawData();
+
+        target_info.load_op = SDL_GPU_LOADOP_LOAD;
+        ImGui_ImplSDLGPU3_PrepareDrawData(draw_data, command_buffer);
+        SDL_GPURenderPass *imgui_render_pass = SDL_BeginGPURenderPass(command_buffer, &target_info, 1, NULL);
+        ImGui_ImplSDLGPU3_RenderDrawData(draw_data, command_buffer, imgui_render_pass, NULL);
+        SDL_EndGPURenderPass(imgui_render_pass);
 
         SDL_GPUFence *fence = SDL_SubmitGPUCommandBufferAndAcquireFence(command_buffer);
         if (!SDL_WaitForGPUFences(gpu, true, &fence, 1)) {
             SDL_PRINT_ERROR_AND_EXIT("Wait for Gpu Fences failed");
         }
+        SDL_ReleaseGPUFence(gpu, fence);
     }
 
+    SDL_WaitForGPUIdle(gpu);
+    ImGui_ImplSDL3_Shutdown();
+    ImGui_ImplSDLGPU3_Shutdown();
+    igDestroyContext(NULL);
+
+    SDL_ReleaseWindowFromGPUDevice(gpu, window);
+    SDL_DestroyGPUDevice(gpu);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
