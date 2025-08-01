b4020e5e8f9f75890207060481f85f675bb3809c
Nathaniel Salvador de Oliveira <nathanieloliveira@yahoo.com.br>
2025-08-01 20:40:43 -0300

    implemented spirv shader reflection

--- main.c
+++ main.c
@@ -23,22 +23,26 @@
         }\
     } while (false)
 
-static void error_callback(void *userdata, const char *error)
-{
-    (void)userdata;
+typedef struct {
+    SDL_GPUShaderCreateInfo info;
+    SDL_GPUShader *shader;
+} shader_t;
+
+static void error_callback(void *userdata, const char *error) {
+    (void) userdata;
     fprintf(stderr, "Error: %s\n", error);
     exit(1);
 }
 
-static void dump_resource_list(spvc_compiler compiler, spvc_resources resources, spvc_resource_type type, const char *tag)
-{
+static void
+dump_resource_list(spvc_compiler compiler, spvc_resources resources, spvc_resource_type type, const char *tag) {
     const spvc_reflected_resource *list = NULL;
     size_t count = 0;
     size_t i;
-    CHECK_RET(spvc_resources_get_resource_list_for_type(resources, type, &list, &count), SPVC_SUCCESS, "Failed to get resource list for type: %d", type);
+    CHECK_RET(spvc_resources_get_resource_list_for_type(resources, type, &list, &count), SPVC_SUCCESS,
+              "Failed to get resource list for type: %d", type);
     printf("%s\n", tag);
-    for (i = 0; i < count; i++)
-    {
+    for (i = 0; i < count; i++) {
         printf("ID: %u, BaseTypeID: %u, TypeID: %u, Name: %s\n", list[i].id, list[i].base_type_id, list[i].type_id,
                list[i].name);
         printf("  Set: %u, Binding: %u\n",
@@ -47,8 +51,7 @@ static void dump_resource_list(spvc_compiler compiler, spvc_resources resources,
     }
 }
 
-static void dump_resources(spvc_compiler compiler, spvc_resources resources)
-{
+static void dump_resources(spvc_compiler compiler, spvc_resources resources) {
     dump_resource_list(compiler, resources, SPVC_RESOURCE_TYPE_UNIFORM_BUFFER, "UBO");
     dump_resource_list(compiler, resources, SPVC_RESOURCE_TYPE_STORAGE_BUFFER, "SSBO");
     dump_resource_list(compiler, resources, SPVC_RESOURCE_TYPE_PUSH_CONSTANT, "Push");
@@ -62,28 +65,46 @@ static void dump_resources(spvc_compiler compiler, spvc_resources resources)
 }
 
 
-SDL_GPUShader* load_shader(SDL_GPUDevice *device, const char *path, SDL_GPUShaderStage stage) {
+shader_t *load_shader(SDL_GPUDevice *device, const char *path, SDL_GPUShaderStage stage, spvc_context context) {
     size_t size = 0;
-    void* file = SDL_LoadFile(path, &size);
-    if (file == NULL) {
+    void *file = SDL_LoadFile(path, &size);
+    if (size == 0) {
+        SDL_free(file);
         return NULL;
     }
 
-    spvc_context context;
     spvc_parsed_ir ir;
     spvc_compiler compiler;
     spvc_compiler_options options;
     spvc_resources resources;
-    CHECK_RET(spvc_context_create(&context), SPVC_SUCCESS, "Failed to create spvc_context");
-    spvc_context_set_error_callback(context, error_callback, NULL);
-    CHECK_RET(spvc_context_parse_spirv(context, file, size / sizeof(SpvId), &ir), SPVC_SUCCESS, "Failed to parse spirv ir");
-    CHECK_RET(spvc_context_create_compiler(context, SPVC_BACKEND_NONE, ir, SPVC_CAPTURE_MODE_TAKE_OWNERSHIP, &compiler), SPVC_SUCCESS, "Failed to create SPVC_BACKEND_NONE compiler");
-    CHECK_RET(spvc_compiler_create_compiler_options(compiler, &options), SPVC_SUCCESS, "Failed to create compiler options");
-    CHECK_RET(spvc_compiler_install_compiler_options(compiler, options), SPVC_SUCCESS, "Failed to install compiler options");
-    CHECK_RET(spvc_compiler_create_shader_resources(compiler, &resources), SPVC_SUCCESS, "Failed to create shader resources");
-    dump_resources(compiler, resources);
+    CHECK_RET(spvc_context_parse_spirv(context, file, size / sizeof(SpvId), &ir), SPVC_SUCCESS,
+              "Failed to parse spirv ir");
+    CHECK_RET(spvc_context_create_compiler(context, SPVC_BACKEND_NONE, ir, SPVC_CAPTURE_MODE_TAKE_OWNERSHIP, &compiler),
+              SPVC_SUCCESS, "Failed to create SPVC_BACKEND_NONE compiler");
+    CHECK_RET(spvc_compiler_create_compiler_options(compiler, &options), SPVC_SUCCESS,
+              "Failed to create compiler options");
+    CHECK_RET(spvc_compiler_install_compiler_options(compiler, options), SPVC_SUCCESS,
+              "Failed to install compiler options");
+    CHECK_RET(spvc_compiler_create_shader_resources(compiler, &resources), SPVC_SUCCESS,
+              "Failed to create shader resources");
 
-    spvc_context_release_allocations(context);
+    size_t num_uniform_buffers;
+    const spvc_reflected_resource *list = NULL;
+    CHECK_RET(spvc_resources_get_resource_list_for_type(resources, SPVC_RESOURCE_TYPE_UNIFORM_BUFFER, &list,
+                                                        &num_uniform_buffers), SPVC_SUCCESS,
+              "Failed to get resource list for type: %d", SPVC_RESOURCE_TYPE_UNIFORM_BUFFER);
+    size_t num_storage_buffers;
+    CHECK_RET(spvc_resources_get_resource_list_for_type(resources, SPVC_RESOURCE_TYPE_STORAGE_BUFFER, &list,
+                                                        &num_storage_buffers), SPVC_SUCCESS,
+              "Failed to get resource list for type: %d", SPVC_RESOURCE_TYPE_STORAGE_BUFFER);
+    size_t num_samplers;
+    CHECK_RET(spvc_resources_get_resource_list_for_type(resources, SPVC_RESOURCE_TYPE_SEPARATE_SAMPLERS, &list,
+                                                        &num_samplers), SPVC_SUCCESS,
+              "Failed to get resource list for type: %d", SPVC_RESOURCE_TYPE_SEPARATE_SAMPLERS);
+    size_t num_storage_textures;
+    CHECK_RET(spvc_resources_get_resource_list_for_type(resources, SPVC_RESOURCE_TYPE_STORAGE_IMAGE, &list,
+                                                        &num_storage_textures), SPVC_SUCCESS,
+              "Failed to get resource list for type: %d", SPVC_RESOURCE_TYPE_STORAGE_IMAGE);
 
     SDL_PropertiesID props = SDL_CreateProperties();
     SDL_SetStringProperty(props, SDL_PROP_GPU_SHADER_CREATE_NAME_STRING, path);
@@ -94,10 +115,20 @@ SDL_GPUShader* load_shader(SDL_GPUDevice *device, const char *path, SDL_GPUShade
             .format = SDL_GPU_SHADERFORMAT_SPIRV,
             .stage = stage,
             .props = props,
+            .num_samplers = num_samplers,
+            .num_storage_buffers = num_storage_buffers,
+            .num_uniform_buffers = num_uniform_buffers,
+            .num_storage_textures = num_storage_textures,
     };
     SDL_GPUShader *shader = SDL_CreateGPUShader(device, &info);
     SDL_free(file);
-    return shader;
+    if (shader == NULL) {
+        return NULL;
+    }
+    shader_t *ret = SDL_malloc(sizeof(shader_t));
+    ret->shader = shader;
+    ret->info = info;
+    return ret;
 }
 
 int main(void) {
@@ -105,9 +136,14 @@ int main(void) {
         SDL_PRINT_ERROR_AND_EXIT("SDL_Init");
     }
 
+    spvc_context context;
+    CHECK_RET(spvc_context_create(&context), SPVC_SUCCESS, "Failed to create spvc_context");
+    spvc_context_set_error_callback(context, error_callback, NULL);
+
     float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
     SDL_WindowFlags window_flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY;
-    SDL_Window* window = SDL_CreateWindow("Kingdom Defense", (int)(800 * main_scale), (int)(600 * main_scale), window_flags);
+    SDL_Window *window = SDL_CreateWindow("Kingdom Defense", (int) (800 * main_scale), (int) (600 * main_scale),
+                                          window_flags);
     if (window == NULL) {
         SDL_PRINT_ERROR_AND_EXIT("Failed to create window");
     }
@@ -117,24 +153,28 @@ int main(void) {
         SDL_PRINT_ERROR_AND_EXIT("Create gpu failed");
     }
 
-    SDL_GPUShader *simple_vert = load_shader(gpu, "simple.frag.hlsl.spirv", SDL_GPU_SHADERSTAGE_VERTEX);
+    shader_t *simple_frag = load_shader(gpu, "simple.frag.hlsl.spirv", SDL_GPU_SHADERSTAGE_FRAGMENT, context);
+    if (simple_frag == NULL) {
+        SDL_PRINT_ERROR_AND_EXIT("failed to load simple_frag shader");
+    }
+
+    shader_t *simple_vert = load_shader(gpu, "simple.vert.hlsl.spirv", SDL_GPU_SHADERSTAGE_VERTEX, context);
     if (simple_vert == NULL) {
         SDL_PRINT_ERROR_AND_EXIT("failed to load simple_vert shader");
     }
-
 
     if (!SDL_ClaimWindowForGPUDevice(gpu, window)) {
         SDL_PRINT_ERROR_AND_EXIT("Failed to claim window for gpu device");
     }
 
     igCreateContext(NULL);
-    ImGuiIO* io = igGetIO_Nil();
+    ImGuiIO *io = igGetIO_Nil();
     io->ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     io->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
     igStyleColorsDark(NULL);
 
-    ImGuiStyle* style = igGetStyle();
+    ImGuiStyle *style = igGetStyle();
     ImGuiStyle_ScaleAllSizes(style, main_scale);
     style->FontScaleDpi = main_scale;
 
@@ -145,6 +185,22 @@ int main(void) {
             .ColorTargetFormat = SDL_GetGPUSwapchainTextureFormat(gpu, window),
     };
     ImGui_ImplSDLGPU3_Init(&imgui_init_info);
+
+    SDL_GPUGraphicsPipelineCreateInfo pipelineCreateInfo = {
+            .vertex_shader = simple_vert->shader,
+            .fragment_shader = simple_frag->shader,
+            .primitive_type = SDL_GPU_PRIMITIVETYPE_TRIANGLELIST,
+            .target_info = {
+                    .num_color_targets = 1,
+                    .color_target_descriptions = (SDL_GPUColorTargetDescription[]){{
+                        .format = SDL_GetGPUSwapchainTextureFormat(gpu, window),
+                    }},
+            },
+            .rasterizer_state = {
+                    .fill_mode = SDL_GPU_FILLMODE_FILL,
+            },
+    };
+    SDL_GPUGraphicsPipeline *pipeline = SDL_CreateGPUGraphicsPipeline(gpu, &pipelineCreateInfo);
 
     bool quit = false;
 
@@ -159,9 +215,9 @@ int main(void) {
             // process events
         }
 
-        /*if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) {
+        if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) {
             continue;
-        }*/
+        }
 
         SDL_GPUCommandBuffer *command_buffer = SDL_AcquireGPUCommandBuffer(gpu);
 
@@ -174,12 +230,16 @@ int main(void) {
 
         SDL_GPUColorTargetInfo target_info = {
                 .texture = window_texture,
-                .clear_color = (SDL_FColor) { .r = 0.1f, .g = 0.1f, .b = 0.1f, .a = 1.0f },
+                .clear_color = (SDL_FColor) {.r = 0.1f, .g = 0.1f, .b = 0.1f, .a = 1.0f},
                 .mip_level = 0,
                 .load_op = SDL_GPU_LOADOP_CLEAR,
                 .store_op = SDL_GPU_STOREOP_STORE,
         };
         SDL_GPURenderPass *render_pass = SDL_BeginGPURenderPass(command_buffer, &target_info, 1, NULL);
+
+        SDL_BindGPUGraphicsPipeline(render_pass, pipeline);
+        SDL_DrawGPUPrimitives(render_pass, 3, 1, 0, 0);
+
         SDL_EndGPURenderPass(render_pass);
 
         // draw imgui
@@ -223,9 +283,12 @@ int main(void) {
     ImGui_ImplSDLGPU3_Shutdown();
     igDestroyContext(NULL);
 
+    SDL_free(simple_frag);
+
     SDL_ReleaseWindowFromGPUDevice(gpu, window);
     SDL_DestroyGPUDevice(gpu);
     SDL_DestroyWindow(window);
     SDL_Quit();
+    spvc_context_destroy(context);
     return 0;
 }
--- shaders/simple.frag.hlsl
+++ shaders/simple.frag.hlsl
@@ -1,16 +1,4 @@
-Texture2DArray myTexture;
-SamplerState mySampler;
-
-struct VS_OUTPUT
+float4 main(float4 Color : TEXCOORD0) : SV_Target0
 {
-    float4 Position : SV_POSITION;
-    float2 TexCoord : TEXCOORD0;
-    uint ArraySliceIndex : TEXCOORD1; // Pass the desired array slice index
-};
-
-float4 main(VS_OUTPUT input) : SV_TARGET
-{
-    // Sample the texture array using the provided texture coordinates and array slice index
-    float4 color = myTexture.Sample(mySampler, float3(input.TexCoord, input.ArraySliceIndex));
-    return color;
+    return Color;
 }
